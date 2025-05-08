8be6b561a643604fd6801f46fe2937d32ced895d
Octavi <122990264+bassegoda@users.noreply.github.com>
2025-05-08 15:21:36 +0200

    Update admissions.sql

--- queries/admissions.sql
+++ queries/admissions.sql
@@ -7,7 +7,7 @@ WITH movement_data AS (
         TIMESTAMPDIFF(hour, m.start_date, m.end_date) AS horesingres,
         LEAD(m.start_date) OVER (PARTITION BY m.episode_ref ORDER BY m.start_date) AS next_start_date
     FROM
-        g_mov_events as m
+        g_movements as m
     WHERE 
         ou_loc_ref IN ('E073', 'I073') 
         AND start_date BETWEEN '2023-01-01 00:00:00' AND '2023-12-31 23:59:59'
